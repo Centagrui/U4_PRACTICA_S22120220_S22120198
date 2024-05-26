687372332fa5d81ded346225deab9b7329ada717
Centagrui <135802065+Centagrui@users.noreply.github.com>
2024-05-25 22:39:55 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+//Maria Centagrui Perez Alvarez
+//Alejandro Perez Vazquez
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
